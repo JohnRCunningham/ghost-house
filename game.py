7b7d2443d971790063ae46d1bea4c2f24253a208
john <john.cunningham@kearsrage.org>
2014-12-04 09:29:09 -0500

    Started coding

--- game.py
+++ game.py
@@ -1,1 +1,21 @@
+import pygame, sys, random
+import player
+import level
+import HUD
+import score
+import menu
+import enemyAI
 
+pygame.init()
+
+clock= pygame.time.Clock()
+
+width = 1200 
+height = 600
+size = width, height
+
+bgColor = r,g,b = 225, 10, 10
+
+screen = pygame.display.set_mode(size)
+
+player = PlayerBall([width/2, height/2])
